5056d8cf271e4d14f13ce99d0c2998980373ce4d
hysamello <62876842+hysamello@users.noreply.github.com>
2020-12-13 08:49:18 +0000

    nothing

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ void signal_handler_2(int signal);
 void AJPseudoEvolutiveAdvancedVersionTest(int size, int path[size],int matrix[size][size], int iterations);
 void AJPseudoEvolutiveBaseVersion(int size, int path[size],int matrix[size][size], int iterations);
 
-int it, pid;
+int it, pid, (*map)[5], *pids;
 
 int main(){
 
@@ -179,6 +179,7 @@ void AJPseudoEvolutiveBaseVersion(int size, int path[size],int matrix[size][size
 
 void signal_handler(int signal){
     printf("Parent: Handling SIGUSR1 in process #%d with PID=%d\n", it, pid);
+    
     kill(pid, SIGUSR2);
     /* for (int it=0; it<5; it++)
         kill(pids[it], SIGUSR2); */
@@ -192,11 +193,12 @@ void signal_handler(int signal){
 
 void signal_handler_2(int signal){
     printf("Child: Handling SIGUSR2 in process #%d with PID=%d\n", it, getpid());
-    /* for(int i=0;i<*pidsMem.length;i++){
-        *map[*pidsMem[i]][0] = *map[getpid()][0];
+
+    /* for(int i=0;i<*pids.length;i++){
+        map[*pidsMem[i]][0] = map[getpid()][0];
     } */
 
-    exit(0);
+    //exit(0);
 }
 
 void AJPseudoEvolutiveAdvancedVersionTest(int size, int path[size],int matrix[size][size], int iterations){
@@ -206,7 +208,8 @@ void AJPseudoEvolutiveAdvancedVersionTest(int size, int path[size],int matrix[si
     int protection = PROT_READ | PROT_WRITE;
     int visibility = MAP_ANONYMOUS | MAP_SHARED;
 
-    int (*map)[5] = (int(*)[5])mmap(NULL, mapSize, protection, visibility, 0, 0);
+    map = (int(*)[5])mmap(NULL, mapSize, protection, visibility, 0, 0);
+    /* pids = mmap(NULL, distSize, protection, visibility, 0, 0); */
     int *dist = mmap(NULL, distSize, protection, visibility, 0, 0);
 
     sem_unlink("job_ready");
@@ -234,9 +237,7 @@ void AJPseudoEvolutiveAdvancedVersionTest(int size, int path[size],int matrix[si
 
         if (pids[it] == 0) {
             printf("Child #%d, PID=%d\n", it, getpid());
-            while (1) {
-                //sem_wait(job_ready);
-                swap(size,path);
+            /* swap(size,path);
                 distAux = calculateDist(5,path,matrix);
                 if(distAux<*dist){
                     map[it][0] = *path;
@@ -244,9 +245,21 @@ void AJPseudoEvolutiveAdvancedVersionTest(int size, int path[size],int matrix[si
                     printf("Distance: %d \n",distAux);
                     kill(getppid(), SIGUSR1);
                 } 
+            while(1); */
+            while (1) {
+                //sem_wait(job_ready);
+                swap(size,path);
+                distAux = calculateDist(5,path,matrix);
+                if(distAux<*dist){
+                    *dist = distAux;
+                    map[it][0] = *path;
+                    pathPrint(5,path);
+                    printf("Distance: %d \n",distAux);
+                    kill(getppid(), SIGUSR1);
+                } 
                 //sem_post(job_done);
             }
-            
+            //exit(0);
         }
         sleep(1);
     }
@@ -268,97 +281,6 @@ void AJPseudoEvolutiveAdvancedVersionTest(int size, int path[size],int matrix[si
         printf("Parent: child with PID=%d ended\n", pid);
     }
 }
-
-/* void AJPseudoEvolutiveAdvancedVersion(int size, int path[size],int matrix[size][size], int iterations){
-    // Create shared memory map
-    int memSize = sizeof(int);
-    int pathMemSize = size * sizeof(int);
-    int mapSize = size * sizeof(*int);
-
-
-    int protection = PROT_READ | PROT_WRITE;
-    int visibility = MAP_ANONYMOUS | MAP_SHARED;
-    int *dist = mmap(NULL, memSize, protection, visibility, 0, 0);
-    int *pathMem = mmap(NULL, pathMemSize, protection, visibility, 0, 0);
-    //int *pidsMem = mmap(NULL, pathMemSize, protection, visibility, 0, 0);
-
-    int *map[] = mmap(NULL, mapSize, protection, visibility, 0, 0);
-
-    signal(SIGUSR1, signal_handler);
-    signal(SIGUSR2, signal_handler_2);
-
-    *dist = __INT_MAX__;
-    //*dist = calculateDist(size, path, matrix);
-    printf("%d\n",*dist);
-    
-    // Create semaphores
-    sem_unlink("job_ready");
-    sem_unlink("job_done");
-    sem_t *job_ready = sem_open("job_ready", O_CREAT, 0644, 0);
-    sem_t *job_done = sem_open("job_done", O_CREAT, 0644, 0);
-    
-    // Workers
-    int num_workers = 5;
-    int pids[num_workers];
-
-    createRandomPath(size,path);
-    int distAux = 0;
-    
-    // Fork worker processes
-    for (int i=0; i<num_workers; i++) {
-        //pids[i] = fork();
-        pidsMem[i] = fork();
-        
-        if (pids[i] == 0) {
-            printf("Worker process #%d!\n", i);
-            int internPath = path;
-            while (1) {
-                sem_wait(job_ready);
-                
-                swap(size,map[i][0]);   
-                distAux = calculateDist(size, map[i][0], matrix);
-                internPath = map[i][0];
-                if(distAux<*dist){
-                    *dist = distAux;
-                    kill(getppid(),SIGUSR1);
-                    copyArray(size,pathMem,path);
-                } 
-                sem_post(job_done);
-            }
-            exit(0);
-        }
-    }
-
-    
-    
-    // Parent process
-    printf("Parent process!\n");
-
-    
-
-    //char *msg[3] = {"Hello World", "Hi", "Waterfall"};
-    for (int i=0; i<iterations; i++) {
-        //strcpy(dist, msg[i]);
-        sem_post(job_ready);
-        sem_wait(job_done);
-    }
-    
-    
-    // Release semaphores
-    sem_close(job_ready);
-    sem_close(job_done);
-    
-    // Kill worker processes
-    for (int i=0; i<num_workers; i++) {
-        printf("Killing %d\n", pids[i]);
-        kill(pids[i], SIGKILL);
-    }    
-
-    pathPrint(size,pathMem);
-    printf("Distance: %d\n", *dist);
-}
- */
-
 
 void copyArray(int size, int arr1[size], int arr2[size]){
     for(int i=0;i<size;i++){
